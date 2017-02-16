609c5760666a6e69e2ea87a92a851a245f8463d7
neb330@nyu.edu <neb330@nyu.edu>
2017-02-16 13:17:09 -0500

    some changes to vae.py

--- scripts/vae.py
+++ scripts/vae.py
@@ -1,24 +1,65 @@
+from __future__ import print_function
 import torch
 import torch.nn.functional as nn
+import torchvision.transforms as transforms
 import torch.autograd as autograd
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
+import pickle
+import argparse
 from torch.autograd import Variable
 from tensorflow.examples.tutorials.mnist import input_data
 
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=10, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
-mb_size = 64
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+
+print('loading data!')
+path = '../data/'
+trainset_labeled = pickle.load(open(path + "train_labeled.p", "rb"))
+#trainset_unlabeled = pickle.load(open(path + "train_unlabeled.p", "rb"))
+train_loader = torch.utils.data.DataLoader(trainset_labeled, batch_size=10, shuffle=True, **kwargs)
+train_final_load = torch.utils.data.DataLoader(trainset_labeled, batch_size=10, shuffle=True, **kwargs)
+print('done')
+
+
+
+#mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
+mb_size = 10
 Z_dim = 100
-X_dim = mnist.train.images.shape[1]
-y_dim = mnist.train.labels.shape[1]
+X_dim = 784
+y_dim = 10
 h_dim = 128
 c = 0
 lr = 1e-3
-
 
 
 def xavier_init(size):
@@ -73,10 +114,9 @@ params = [Wxh, bxh, Whz_mu, bhz_mu, Whz_var, bhz_var,
 
 solver = optim.Adam(params, lr=lr)
 
-for it in range(10):
-    X, _ = mnist.train.next_batch(mb_size)
-    X = Variable(torch.from_numpy(X))
-    print(X.size())
+for batch_idx, (data, target) in enumerate(train_loader):
+    X = data.view(mb_size, 784)
+    X = Variable(X)
     
     # Forward
     z_mu, z_var = Q(X)
@@ -99,8 +139,8 @@ for it in range(10):
         p.grad.data.zero_()
     
     # Print and plot every now and then
-    if it % 1000 == 0:
-        print('Iter-{}; Loss: {:.4}'.format(it, loss.data[0]))
+    if batch_idx % 1000 == 0:
+        print('Iter-{}; Loss: {:.4}'.format(batch_idx, loss.data[0]))
         
         samples = P(z).data.numpy()[:16]
         
@@ -122,3 +162,20 @@ for it in range(10):
         plt.savefig('out/{}.png'.format(str(c).zfill(3)), bbox_inches='tight')
         c += 1
         plt.close(fig)
+
+arr = np.zeros((3000, Z_dim))
+for batch_idx, (data, target) in enumerate(train_final_load):
+    X = data.view(mb_size, 784)
+    X = Variable(X)
+    
+    # Forward
+    z_mu, z_var = Q(X)
+    z = sample_z(z_mu, z_var)
+    #new_z = z.asNumpyTensor()
+    print(new_z.shape)
+#np.append(arr, np.array(z), axis=0)
+
+
+
+
+
